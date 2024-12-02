11f199f7d54ff390a249affd00da5f20e88a1d23
Dan0227 <jhojan0227@gmail.com>
2024-12-02 14:00:04 -0500

    Front Regis and log Finished

--- app/index.js
+++ app/index.js
@@ -1,1 +1,18 @@
-console.log('Hola mundo')
+import express from "express";
+//Fix para __dirname
+import path from "path"
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+//Server
+const app = express();
+app.set("port", 4000);
+app.listen(app.get("port"));
+console.log(`Server port ${app.get("port")}`);
+
+//Configuración
+app.use(express.static(__dirname + "/public"))
+
+//Rutas
+app.get("/",(req, res)=> res.sendFile(__dirname + "/pages/login.html"))
+app.get("/register",(req, res)=> res.sendFile(__dirname + "/pages/register.html"))
